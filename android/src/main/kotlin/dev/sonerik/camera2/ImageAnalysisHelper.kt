2a1f769fccad8329f36159a0613d2d18f943d14e
Alex Isaienko <thesonerik@gmail.com>
2020-11-11 19:17:43 +0200

    ImageAnalysisHelper.kt

---
+++ android/src/main/kotlin/dev/sonerik/camera2/ImageAnalysisHelper.kt
@@ -0,0 +1,126 @@
+package dev.sonerik.camera2
+
+import android.content.Context
+import android.graphics.*
+import android.util.Size
+import androidx.camera.core.ImageProxy
+import java.nio.ByteBuffer
+
+enum class ColorOrder {
+    RGB, BGR
+}
+
+enum class Normalization {
+    UBYTE, BYTE, UFLOAT, FLOAT
+}
+
+class ImageAnalysisHelper(
+        context: Context,
+        private val targetSize: Size,
+        private val colorOrder: ColorOrder,
+        private val normalization: Normalization
+) {
+    private lateinit var analysisBitmap: Bitmap
+
+    private var targetRotationDegrees: Int = 0
+    private lateinit var targetBitmap: Bitmap
+
+    private val yuvToRgbConverter = YuvToRgbConverter(context)
+
+    private val outBuffer = ByteBuffer.allocate(targetSize.width * targetSize.height * 3 * 4)
+
+    private var _lastFrame: ByteArray? = null
+    val lastFrame: ByteArray?
+        get() = _lastFrame
+
+    fun getAnalysisFrame(image: ImageProxy): ByteArray {
+        if (!::analysisBitmap.isInitialized || analysisBitmap.width != image.width || analysisBitmap.height != image.height) {
+            analysisBitmap = Bitmap.createBitmap(image.width, image.height, Bitmap.Config.ARGB_8888)
+        }
+        yuvToRgbConverter.yuvToRgb(image, analysisBitmap)
+
+        if (!::targetBitmap.isInitialized || targetRotationDegrees != image.imageInfo.rotationDegrees) {
+            targetRotationDegrees = image.imageInfo.rotationDegrees
+            targetBitmap = createTargetBitmap(
+                    targetSize = targetSize,
+                    rotationDegrees = image.imageInfo.rotationDegrees
+            )
+        }
+
+        writeScaledRotatedBitmap(analysisBitmap, targetBitmap)
+        writeAnalyzableBitmapToBuffer(targetBitmap, outBuffer)
+        val frame = outBuffer.array()
+        _lastFrame = frame
+        return frame
+    }
+
+    private fun writeAnalyzableBitmapToBuffer(bitmap: Bitmap, buffer: ByteBuffer) {
+        buffer.rewind()
+        val colors = IntArray(3) { 0 }
+        for (y in 0 until bitmap.width) {
+            for (x in 0 until bitmap.height) {
+                val px = bitmap.getPixel(x, y)
+
+                // Get channel values from the pixel value.
+                when (colorOrder) {
+                    ColorOrder.RGB -> {
+                        colors[0] = Color.red(px)
+                        colors[1] = Color.green(px)
+                        colors[2] = Color.blue(px)
+                    }
+                    ColorOrder.BGR -> {
+                        colors[0] = Color.blue(px)
+                        colors[1] = Color.green(px)
+                        colors[2] = Color.red(px)
+                    }
+                }
+
+                // Normalize channel values.
+                //
+                // Ranges:
+                // UBYTE: [0, 255]
+                // BYTE: [-127, 127]
+                // UFLOAT: [0.0, 1.0]
+                // FLOAT: [-1.0, 1.0]
+                //
+                when (normalization) {
+                    Normalization.UBYTE -> {
+                        buffer.put(colors[0].toByte())
+                        buffer.put(colors[1].toByte())
+                        buffer.put(colors[2].toByte())
+                    }
+                    Normalization.BYTE -> {
+                        buffer.put((colors[0] - 127).toByte())
+                        buffer.put((colors[1] - 127).toByte())
+                        buffer.put((colors[2] - 127).toByte())
+                    }
+                    Normalization.UFLOAT -> {
+                        buffer.putFloat(colors[0] / 255f)
+                        buffer.putFloat(colors[1] / 255f)
+                        buffer.putFloat(colors[2] / 255f)
+                    }
+                    Normalization.FLOAT -> {
+                        buffer.putFloat((colors[0] - 127) / 127f)
+                        buffer.putFloat((colors[1] - 127) / 127f)
+                        buffer.putFloat((colors[2] - 127) / 127f)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createTargetBitmap(targetSize: Size, rotationDegrees: Int): Bitmap {
+        val swapWithHeight = rotationDegrees == 90 || rotationDegrees == 270
+        return Bitmap.createBitmap(
+                if (!swapWithHeight) targetSize.width else targetSize.height,
+                if (!swapWithHeight) targetSize.height else targetSize.width,
+                Bitmap.Config.ARGB_8888
+        )
+    }
+
+    private fun writeScaledRotatedBitmap(source: Bitmap, target: Bitmap) {
+        val canvas = Canvas(target)
+        canvas.rotate(targetRotationDegrees.toFloat(), target.width.toFloat() / 2, target.height.toFloat() / 2)
+        canvas.drawBitmap(source, null, Rect(0, 0, target.width, target.height), null)
+    }
+}
